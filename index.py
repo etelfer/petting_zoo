c9a17f7cc2ca91ee9104396588bf2b343d5784b9
Erin Telfer <eringtelfer@gmail.com>
2026-01-17 12:35:00 -0600

    initial commit

---
+++ index.py
@@ -0,0 +1,138 @@
+# import the python datetime module to help us create a timestamp
+from datetime import date
+
+class Copperhead:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class RatSnake:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class Anaconda:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class CornSnake:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class Salamander:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.slithering = True
+
+class Mallard:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class Goldfish:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class Koi:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class Goose:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+class Swan:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.swimming = True
+
+miss_fuzz = Llama("Miss Fuzz", "Domestic Llama")
+
+eeyore = Donkey("Eeyore", "Donkey")
+
+vincent = Goat("Vincent", "Pygmy Goat")
+
+marty = Zebra("Marty", "Plains Zebra")
+
+shaun = Sheep("Shaun", "Merino Sheep")
+
+bitey = Copperhead("Bitey", "Southern Copperhead")
+
+wilbur = RatSnake("Wilbur", "Black Rat Snake")
+
+stretchy = Anaconda("Stretchy", "Green Anaconda")
+
+popcorn = CornSnake("Popcorn", "Amelastic Corn Snake")
+
+sally = Salamander("Sally", "Spotted Salamander")
+
+quackers = Mallard("Quackers", "Mallard Duck")
+
+goldie = Goldfish("Goldie", "Common Goldfish")
+
+kenji = Koi("Kenji", "Butterfly Koi")
+
+ryan = Goose("Ryan", "Canada Goose")
+
+odette = Swan("Odette", "Mute Swan")
+ 
+print(miss_fuzz.name)
+print(eeyore.name)
+print(vincent.name)
+print(marty.name)
+print(shaun.name)
+print(bitey.name)
+print(wilbur.name)
+print(stretchy.name)
+print(popcorn.name)
+print(sally.name)
+print(quackers.name)
+print(goldie.name)
+print(kenji.name)
+print(ryan.name)
+print(odette.name)
---
+++ walking/goat.py
@@ -0,0 +1,8 @@
+class Goat: 
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.walking = True
