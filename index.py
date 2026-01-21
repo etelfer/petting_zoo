ec7979d0bb93962938c4c37fc16af602c14378bb
Erin Telfer <eringtelfer@gmail.com>
2026-01-20 19:57:26 -0600

    added feed methos and __str__

--- index.py
+++ index.py
@@ -5,37 +5,38 @@ from swimming import Goldfish, Goose, Koi, Mallard, Swan
 from walking import Donkey, Goat, Llama, Sheep, Zebra
 
 
-miss_fuzz = Llama("Miss Fuzz", "Domestic Llama", "midday")
+miss_fuzz = Llama("Miss Fuzz", "Domestic Llama", "midday", "Llama Chow")
 
-eeyore = Donkey("Eeyore", "Donkey", "morning")
+eeyore = Donkey("Eeyore", "Donkey", "morning", "Donkey Chow")
 
-vincent = Goat("Vincent", "Pygmy Goat", "afternoon")
+vincent = Goat("Vincent", "Pygmy Goat", "afternoon", "Goat Chow")
 
-marty = Zebra("Marty", "Plains Zebra", "midday")
+marty = Zebra("Marty", "Plains Zebra", "midday", "Zebra Chow")
 
-shaun = Sheep("Shaun", "Merino Sheep", "morning")
+shaun = Sheep("Shaun", "Merino Sheep", "morning", "Sheep Chow")
 
-bitey = Copperhead("Bitey", "Southern Copperhead")
+bitey = Copperhead("Bitey", "Southern Copperhead", "Mice")
 
-wilbur = RatSnake("Wilbur", "Black Rat Snake")
+wilbur = RatSnake("Wilbur", "Black Rat Snake", "Mice")
 
-stretchy = Anaconda("Stretchy", "Green Anaconda")
+stretchy = Anaconda("Stretchy", "Green Anaconda", "Mice")
 
-popcorn = CornSnake("Popcorn", "Amelastic Corn Snake")
+popcorn = CornSnake("Popcorn", "Amelastic Corn Snake", "Mice")
 
-sally = Salamander("Sally", "Spotted Salamander")
+sally = Salamander("Sally", "Spotted Salamander","Crickets")
 
-quackers = Mallard("Quackers", "Mallard Duck")
+quackers = Mallard("Quackers", "Mallard Duck","Duck Feed")
 
-goldie = Goldfish("Goldie", "Common Goldfish")
+goldie = Goldfish("Goldie", "Common Goldfish", "Fish Food")
 
-kenji = Koi("Kenji", "Butterfly Koi")
+kenji = Koi("Kenji", "Butterfly Koi", "Fish Food")
 
-ryan = Goose("Ryan", "Canada Goose")
+ryan = Goose("Ryan", "Canada Goose", "Goose Feed")
 
-odette = Swan("Odette", "Mute Swan")
+odette = Swan("Odette", "Mute Swan", "Swan Feed")
  
 print(f'{miss_fuzz.name} the {miss_fuzz.species} is available to pet during the {miss_fuzz.shift} shift.')
+miss_fuzz.feed()
 # print(eeyore.name)
 # print(vincent.name)
 # print(marty.name)
